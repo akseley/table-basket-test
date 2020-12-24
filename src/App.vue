cab45f6afed5d4cedcfa2d937fa719204b6602ca
akseley <akseley88@gmail.com>
2020-12-24 18:22:42 +0300

    first test commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,171 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+    >
+      <v-text-field
+          v-model="course"
+          type="number"
+          label="Текущий курс"
+          dense
+      />
+      <v-spacer/>
+      <v-btn @click="showBasket = !showBasket">
+        <v-icon>mdi-basket</v-icon>
+        <v-badge
+            v-if="counterSelectedItems"
+            bottom
+            color="green"
+            :content="counterSelectedItems"
+        />
+      </v-btn>
+
+  </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-layout>
+            <v-expansion-panels v-model="panels" multiple>
+              <v-expansion-panel
+                  v-for="group in formattedData"
+                  :key="group.id"
+              >
+                <v-expansion-panel-header>
+                  {{ group.title }}
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-data-table
+                      :items="group.items"
+                      item-key="id"
+                      hide-default-header
+                      hide-default-footer
+                  >
+                    <template slot='item' slot-scope="{item}">
+                      <tr @click="selectItem(item)">
+                        <td>{{ `${item.name} (${item.maxQuantity})` }}</td>
+                        <td class="text-right">{{ formatPrice(item.price) }}</td>
+                      </tr>
+                    </template>
+                  </v-data-table>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          <div v-if="showBasket" class="basket">
+            <basket :items="selectedItems" @remove="selectItem"/>
+          </div>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from '@/data/data.json'
+import names from '@/data/names.json'
+import Basket from '@/components/Basket'
+
+const COURSE = 80
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  components: {Basket},
+
+  data: () => ({
+    course: COURSE,
+    data: data,
+    names: names,
+    showBasket: false,
+    panels: [0,1,2,3,4,5,6,7],
+    formattedData: [],
+    selectedItems: [],
+  }),
+  mounted() {
+    this.formatData()
+  },
+  computed: {
+    counterSelectedItems() {
+      return this.selectedItems.length
+    }
+  },
+  methods: {
+    convertToRub(price) {
+      return (price * this.course).toFixed(2)
+    },
+    formatPrice(convertedPrice){
+      const price = convertedPrice.toLocaleString('ru-RU')
+      return `${price}р.`
+    },
+    formatData() {
+      const data = this.data?.['Value']?.['Goods'] ?? []
+      this.formattedData = this.groupedArray(data).map(group => {
+        return {
+          id: group.id,
+          title: this.getGroupName(group.id),
+          items: this.translateGroupItems(group.id, group.items)
+        }
+      })
+    },
+    groupedArray(array) {
+      let result = {}
+
+      array.forEach(element => {
+        const { G } = element
+        const key = `${G}`
+
+        if (result[key]) {
+          result[key].items.push(element)
+        } else {
+          result[key] = {
+            id: key,
+            items: [
+              {
+                ...element,
+              },
+            ]
+          }
+        }
+      })
+      return Object.values(result)
+    },
+    getGroupName(id) {
+      return this.names[id].G
+    },
+    translateGroupItems(groupId, array) {
+      return array.map(item => {
+        return {
+          id: item.T,
+          name: this.getItemName(groupId, item.T),
+          price: this.convertToRub(item.C),
+          maxQuantity: item.P,
+          quantity: 1
+        }
+      })
+    },
+    getItemName(groupId, itemId) {
+      return this.names[groupId].B[itemId].N
+    },
+    selectItem(item) {
+      if (this.selectedItems.some(selectedItem => selectedItem.id === item.id)) {
+        this.selectedItems = this.selectedItems.filter(selectedItem => selectedItem.id !== item.id)
+      } else {
+        this.selectedItems.push(item)
+      }
+    },
+  },
+  watch: {
+    course() {
+      this.formatData()
+    }
+  }
+};
+</script>
+<style scoped>
+.basket {
+  position: absolute;
+  width: 50%;
+  min-height: 30%;
+  top: 0;
+  right: 0;
+  z-index: 1;
 }
 </style>
---
+++ src/components/Basket.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Корзина
+    </v-card-title>
+    <v-card-text>
+      <v-data-table
+          :items="items"
+          item-key="id"
+          hide-default-header
+          hide-default-footer
+      >
+        <template slot='item' slot-scope="{item}">
+          <tr>
+            <td>
+              {{item.name}}
+            </td>
+            <td>
+              <v-text-field
+                  v-model="item.quantity"
+                  type="number"
+              />
+            </td>
+            <td>
+              {{ formatPrice(item.price)}}
+            </td>
+            <td>
+              <v-btn @click="$emit('remove', item)">
+                <v-icon>
+                  mdi-delete
+                </v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </template>
+      </v-data-table>
+      <div>total: {{total}}</div>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'Basket',
+
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+
+  computed: {
+    total() {
+      const total = this.items.reduce((sum, item) => sum + Number(item.price * item.quantity), 0).toFixed(2)
+      return this.formatPrice(total)
+    },
+  },
+  methods: {
+    formatPrice(price){
+      const formatedPrice = price.toLocaleString('ru-RU')
+      return `${formatedPrice}р.`
+    },
+  },
+};
+</script>
